142532cb2391b2c71dabdb0e2077a1e4055eef50
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-18 14:00:20 -0500

    used Navbar for routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from '../Navbar';
+import Characters from '../Characters';
 
 const Index = () => <h2>Home</h2>
-const Characters = () => <h2> Characters</h2>
 
 const App = () => (
   <Router>
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/characters/">Characters</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <Navbar />
       <Route path="/" exact component={Index} />
       <Route path="/characters/" component={Characters} />
     </div>
