01b1f3d00ced710a65e3c45e868ebdcacab0e48d
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-18 13:40:27 -0500

    Setup basic routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          DBZ LF2
-        </header>
-      </div>
-    );
-  }
-}
+const Index = () => <h2>Home</h2>
+const Characters = () => <h2> Characters</h2>
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/characters/">Characters</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/characters/" component={Characters} />
+    </div>
+  </Router>
+);
+
 
 export default App;
