471a8426f25832c0e471525a25f7512803ad2c8e
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-22 17:29:37 -0500

    Customized tailwind config to reduce filesize

--- tailwind.js
+++ tailwind.js
@@ -55,39 +55,7 @@ let colors = {
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
   'white': '#ffffff',
-
-  'red-darkest': '#3b0d0c',
-  'red-darker': '#621b18',
-  'red-dark': '#cc1f1a',
-  'red': '#e3342f',
-  'red-light': '#ef5753',
-  'red-lighter': '#f9acaa',
-  'red-lightest': '#fcebea',
-
-  'orange-darkest': '#462a16',
-  'orange-darker': '#613b1f',
-  'orange-dark': '#de751f',
-  'orange': '#f6993f',
-  'orange-light': '#faad63',
-  'orange-lighter': '#fcd9b6',
-  'orange-lightest': '#fff5eb',
-
-  'yellow-darkest': '#453411',
-  'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
-  'yellow': '#ffed4a',
-  'yellow-light': '#fff382',
-  'yellow-lighter': '#fff9c2',
-  'yellow-lightest': '#fcfbeb',
-
-  'green-darkest': '#0f2f21',
-  'green-darker': '#1a4731',
-  'green-dark': '#1f9d55',
-  'green': '#38c172',
-  'green-light': '#51d88a',
-  'green-lighter': '#a2f5bf',
-  'green-lightest': '#e3fcec',
-
+  
   'teal-darkest': '#0d3331',
   'teal-darker': '#20504f',
   'teal-dark': '#38a89d',
@@ -103,29 +71,7 @@ let colors = {
   'blue-light': '#6cb2eb',
   'blue-lighter': '#bcdefa',
   'blue-lightest': '#eff8ff',
-
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
-  'indigo': '#6574cd',
-  'indigo-light': '#7886d7',
-  'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
-
-  'purple-darkest': '#21183c',
-  'purple-darker': '#382b5f',
-  'purple-dark': '#794acf',
-  'purple': '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff',
-
-  'pink-darkest': '#451225',
-  'pink-darker': '#6f213f',
-  'pink-dark': '#eb5286',
-  'pink': '#f66d9b',
-  'pink-light': '#fa7ea8',
-  'pink-lighter': '#ffbbca',
+  
   'pink-lightest': '#ffebef',
 }
 
@@ -170,7 +116,6 @@ module.exports = {
     'sm': '576px',
     'md': '768px',
     'lg': '992px',
-    'xl': '1200px',
   },
 
 
@@ -219,14 +164,6 @@ module.exports = {
       'Liberation Serif',
       'Georgia',
       'serif',
-    ],
-    'mono': [
-      'Menlo',
-      'Monaco',
-      'Consolas',
-      'Liberation Mono',
-      'Courier New',
-      'monospace',
     ],
   },
 
@@ -674,8 +611,6 @@ module.exports = {
     '12': '3rem',
     '16': '4rem',
     '20': '5rem',
-    '24': '6rem',
-    '32': '8rem',
   },
 
 
@@ -710,8 +645,6 @@ module.exports = {
     '12': '3rem',
     '16': '4rem',
     '20': '5rem',
-    '24': '6rem',
-    '32': '8rem',
   },
 
 
@@ -745,8 +678,6 @@ module.exports = {
     '12': '3rem',
     '16': '4rem',
     '20': '5rem',
-    '24': '6rem',
-    '32': '8rem',
   },
 
 
@@ -886,7 +817,7 @@ module.exports = {
   */
 
   modules: {
-    appearance: ['responsive'],
+    appearance: false,
     backgroundAttachment: ['responsive'],
     backgroundColors: ['responsive', 'hover', 'focus'],
     backgroundPosition: ['responsive'],
@@ -900,8 +831,8 @@ module.exports = {
     cursor: ['responsive'],
     display: ['responsive'],
     flexbox: ['responsive'],
-    float: ['responsive'],
-    fonts: ['responsive'],
+    float: false,
+    fonts: [],
     fontWeights: ['responsive', 'hover', 'focus'],
     height: ['responsive'],
     leading: ['responsive'],
@@ -911,7 +842,7 @@ module.exports = {
     maxWidth: ['responsive'],
     minHeight: ['responsive'],
     minWidth: ['responsive'],
-    negativeMargin: ['responsive'],
+    negativeMargin: false,
     objectFit: false,
     objectPosition: false,
     opacity: ['responsive'],
@@ -920,22 +851,22 @@ module.exports = {
     padding: ['responsive'],
     pointerEvents: ['responsive'],
     position: ['responsive'],
-    resize: ['responsive'],
+    resize: false,
     shadows: ['responsive', 'hover', 'focus'],
-    svgFill: [],
-    svgStroke: [],
-    tableLayout: ['responsive'],
+    svgFill: false,
+    svgStroke: false,
+    tableLayout: false,
     textAlign: ['responsive'],
     textColors: ['responsive', 'hover', 'focus'],
     textSizes: ['responsive'],
     textStyle: ['responsive', 'hover', 'focus'],
-    tracking: ['responsive'],
-    userSelect: ['responsive'],
-    verticalAlign: ['responsive'],
+    tracking: [],
+    userSelect: false,
+    verticalAlign: [],
     visibility: ['responsive'],
-    whitespace: ['responsive'],
+    whitespace: [],
     width: ['responsive'],
-    zIndex: ['responsive'],
+    zIndex: [],
   },
 
 
