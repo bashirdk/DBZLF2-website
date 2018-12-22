64ed366783e88a82391342de55b220016a6cd47d
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-22 17:57:54 -0500

    use Switch for better routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import CharacterList from "../CharacterList";
 import CharacterProfile from "../CharacterProfile";
@@ -8,14 +8,18 @@ import Navbar from "../Navbar";
 import React from "react";
 
 const Index = () => <h2>Home</h2>;
+const NoMatch = () => <h2>404: Not found</h2>
 
 const App = () => (
   <Router>
     <>
       <Navbar />
-      <Route path="/" exact component={Index} />
-      <Route path="/characters/" exact component={CharacterList} />
-      <Route path="/characters/:saga/:name" component={CharacterProfile} />
+      <Switch>
+        <Route path="/characters/:saga/:name" component={CharacterProfile} />
+        <Route path="/characters/" exact component={CharacterList} />
+        <Route path="/" exact component={Index} />
+        <Route component={NoMatch} />
+      </Switch>
     </>
   </Router>
 );
