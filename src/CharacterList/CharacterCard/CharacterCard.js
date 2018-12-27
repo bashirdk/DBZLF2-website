ee2ddbb4e1487ebeb28fa7b787bd1046d040f10f
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-27 15:21:39 -0500

    Added some basic styling to the CharacterCard component

--- src/CharacterList/CharacterCard/CharacterCard.js
+++ src/CharacterList/CharacterCard/CharacterCard.js
@@ -3,10 +3,11 @@ import React from "react";
 
 const CharacterCard = ({ character }) => (
   <Link to={`/characters/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}`}>
-    <div>
+    <div className="inline-block text-center border mx-4 my-4">
       <img 
         src={require(`../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.png`)}
         alt={`face pic of ${character.name}`}
+        className="block"
       />
       {character.name}
     </div>
