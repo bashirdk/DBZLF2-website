a413f902332864223921edb8eb35278f4e47aa19
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-27 13:43:17 -0500

    Added face picture to the CharacterCard component

--- src/CharacterList/CharacterCard/CharacterCard.js
+++ src/CharacterList/CharacterCard/CharacterCard.js
@@ -3,7 +3,14 @@ import React from "react";
 
 const CharacterCard = ({ character }) => (
   <Link to={`/characters/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}`}>
-    {character.name}
+    <div>
+      <img 
+        src={require(`../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.png`)}
+        alt={`face pic of ${character.name}`}
+      />
+      {character.name}
+    </div>
+    
   </Link>
 );
 
