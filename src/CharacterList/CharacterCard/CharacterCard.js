6a419375a58f544993ad730e2cf4ff72a45114cf
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-29 11:56:14 -0500

    Updated styles on CharacterCard component

--- src/CharacterList/CharacterCard/CharacterCard.js
+++ src/CharacterList/CharacterCard/CharacterCard.js
@@ -3,13 +3,15 @@ import React from "react";
 
 const CharacterCard = ({ character }) => (
   <Link to={`/characters/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}`}>
-    <div className="inline-block text-center border mx-4 my-4">
+    <div className="inline-block border mx-4 my-4 w-48 rounded overflow-hidden hover:shadow-md hover:font-bold">
       <img 
         src={require(`../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.png`)}
         alt={`face pic of ${character.name}`}
-        className="block"
+        className="w-full"
       />
+      <div class="text-center text-sm mx-2 my-2">
       {character.name}
+      </div>      
     </div>
     
   </Link>
