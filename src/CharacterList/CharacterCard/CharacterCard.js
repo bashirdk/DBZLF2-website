e73778ee5b28fc6ad8b88c572f8379db18f0f204
Bashir Duranai-Khan <bashirkdk@gmail.com>
2018-12-30 22:31:40 -0500

    Added some custom CSS styles to the CharacterCard component

--- src/CharacterList/CharacterCard/CharacterCard.js
+++ src/CharacterList/CharacterCard/CharacterCard.js
@@ -1,15 +1,17 @@
+import "./CharacterCard.css";
+
 import { Link } from "react-router-dom";
 import React from "react";
 
 const CharacterCard = ({ character }) => (
   <Link to={`/characters/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}`}>
-    <div className="inline-block border mx-4 my-4 w-48 rounded overflow-hidden hover:shadow-md hover:font-bold">
+    <div className="character-card inline-block border mx-4 my-4 w-48 rounded overflow-hidden">
       <img 
         src={require(`../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.png`)}
         alt={`face pic of ${character.name}`}
         className="w-full"
       />
-      <div class="text-center text-sm mx-2 my-2">
+      <div class="text-center text-sm mx-1 my-2">
       {character.name}
       </div>      
     </div>
