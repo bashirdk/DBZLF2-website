8182b07606ff26a77481a172458a772acc9424e7
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-07 15:08:56 -0500

    Updated responsive padding of CharacterBio componenet

--- src/CharacterProfile/CharacterProfile.js
+++ src/CharacterProfile/CharacterProfile.js
@@ -24,7 +24,7 @@ class CharacterProfile extends Component {
       <div>
         <h2>Character Profile</h2>
         <h3 className="text-center">{this.state.character.name}</h3>
-        <section className="pr-5 my-5 inline-block align-top w-full sm:w-1/3">
+        <section className="sm:pr-5 my-5 inline-block align-top w-full sm:w-1/3">
           <CharacterBio character={this.state.character} />
         </section>
         <section className="my-5  inline-block w-full sm:w-2/3 ">
