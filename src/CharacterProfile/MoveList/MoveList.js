effd011dd9b09b014136a5d73677475e18373849
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-07 15:47:35 -0500

    Updated styling and responsiveness of MoveList component

--- src/CharacterProfile/MoveList/MoveList.js
+++ src/CharacterProfile/MoveList/MoveList.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const MoveList = ({ character }) => {
   
-  const moveContainerStyle = "border-b";
-  const moveNameStyle = "inline-block w-2/5 border-r px-2 py-2";
-  const moveInputStyle = "inline-block w-3/5 px-2 py-2 align-top";
+  const moveContainerStyle = "border-b sm:flex";
+  const moveHeaderStyle = "border-b px-2 py-2 text-white";
+  const moveNameStyle = "bg-grey-lighter w-full block sm:w-1/2 md:w-2/5 md:border-b-0 px-2 py-2";
+  const moveInputStyle = "w-full block sm:w-1/2 md:w-3/5 px-2 py-2";
 
   const subMoveList = ( input ) => {
     const mapMoves = input.map((mappedMove) => (
@@ -25,16 +26,14 @@ const MoveList = ({ character }) => {
   const counterAttacks = subMoveList(character.moves.counter);
   const transformations = subMoveList(character.moves.transformations);
 
-  const moveHeaderStyle = "border-b px-2 py-2";
-
   return (
     <div className="border rounded border-b-0">
       <h3 className="border-b p-2"> Move List </h3>
 
-      <h4 className={moveHeaderStyle}> Basic Moves </h4> {basicMoves}
-      <h4 className={moveHeaderStyle}> Special Moves </h4> {specialMoves}
-      <h4 className={moveHeaderStyle}> Counter Attacks </h4> {counterAttacks}
-      <h4 className={moveHeaderStyle}> Transformations </h4> {transformations}
+      <h4 className={'bg-blue ' + moveHeaderStyle } > Basic Moves </h4> {basicMoves}
+      <h4 className={'bg-red ' + moveHeaderStyle}> Special Moves </h4> {specialMoves}
+      <h4 className={'bg-green ' + moveHeaderStyle}> Counter Attacks </h4> {counterAttacks}
+      <h4 className={'bg-yellow-dark ' + moveHeaderStyle}> Transformations </h4> {transformations}
     </div>
   );
 };
