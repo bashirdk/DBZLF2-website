7e884f861efdf1b375ec6a9e6b6086ed595af858
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-07 12:40:52 -0500

    Updated borders of MoveList component

--- src/CharacterProfile/MoveList/MoveList.js
+++ src/CharacterProfile/MoveList/MoveList.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const MoveList = ({ character }) => {
   
-  const moveContainerStyle = "border";
-  const moveNameStyle = "inline-block w-2/5 border px-2 py-2";
-  const moveInputStyle = "inline-block w-3/5 border px-2 py-2";
+  const moveContainerStyle = "border-b";
+  const moveNameStyle = "inline-block w-2/5 border-r px-2 py-2";
+  const moveInputStyle = "inline-block w-3/5 px-2 py-2 align-top";
 
   const subMoveList = ( input ) => {
     const mapMoves = input.map((mappedMove) => (
@@ -25,11 +25,11 @@ const MoveList = ({ character }) => {
   const counterAttacks = subMoveList(character.moves.counter);
   const transformations = subMoveList(character.moves.transformations);
 
-  const moveHeaderStyle = "border px-2 py-2";
+  const moveHeaderStyle = "border-b px-2 py-2";
 
   return (
-    <div className="border">
-      <h3> Move List </h3>
+    <div className="border border-b-0">
+      <h3 className="border-b p-2"> Move List </h3>
 
       <h4 className={moveHeaderStyle}> Basic Moves </h4> {basicMoves}
       <h4 className={moveHeaderStyle}> Special Moves </h4> {specialMoves}
