aca784ed2526a389659b080f9771d42d2c30d182
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-08 17:18:39 -0500

    Added basic implementation for the bars for character stats in the CharacterStats component

--- src/CharacterProfile/CharacterStats/CharacterStats.js
+++ src/CharacterProfile/CharacterStats/CharacterStats.js
@@ -4,23 +4,31 @@ import './CharacterStats.css'
 
 const CharacterStats = ({ character }) => {  
 
-  const statContainer = (statName) => {
+  const barStyle = "mx-2 w-full h-full"
+
+  const statContainer = (statName, stat) => {
+
+    const statStyle = {
+      width: stat
+    }
+
     return (
-    <div className="border-b flex">
-    <div className="block sm:inline-block w-24 px-3 py-5 bg-grey-lighter">
+    <div className="flex">
+    <div className="block md:inline-block w-24 px-3 py-3 text-right">
       <p>{statName}</p>
     </div>
-    <div className="block md:inline-block">
-      <div className="bar">
+    <div className="block md:inline-block py-2 pr-5 flex-auto">
+      <div className="mx-2 w-full h-full bg-grey-lighter relative">
+        <span className="inline-block h-full bg-red" style={statStyle}></span>
       </div>
     </div>        
     </div> );
   };
 
-  const attackStat = statContainer("Attack");
-  const defenseStat = statContainer("Defense");
-  const speedStat = statContainer("Speed");
-  const kiStat = statContainer("Ki Usage");
+  const attackStat = statContainer("Attack", character.stats.attack);
+  const defenseStat = statContainer("Defense", character.stats.defense);
+  const speedStat = statContainer("Speed", character.stats.speed);
+  const kiStat = statContainer("Ki Usage", character.stats.ki);
 
   return  (
     <div className="border rounded">
