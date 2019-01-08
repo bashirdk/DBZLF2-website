22e7b13586999b05a56e549582b2930382f5eb9a
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-08 15:39:54 -0500

    Created basic skeleton of the CharacterStats component

--- src/CharacterProfile/CharacterStats/CharacterStats.js
+++ src/CharacterProfile/CharacterStats/CharacterStats.js
@@ -3,8 +3,34 @@ import React from "react";
 import './CharacterStats.css'
 
 const CharacterStats = ({ character }) => {  
+
+  const statContainer = (statName) => {
+    return (
+    <div className="border-b flex">
+    <div className="block sm:inline-block w-24 px-3 py-5 bg-grey-lighter">
+      <p>{statName}</p>
+    </div>
+    <div className="block md:inline-block">
+      <div className="bar">
+      </div>
+    </div>        
+    </div> );
+  };
+
+  const attackStat = statContainer("Attack");
+  const defenseStat = statContainer("Defense");
+  const speedStat = statContainer("Speed");
+  const kiStat = statContainer("Ki Usage");
+
   return  (
-    <p> CHARACTER STATS </p>
+    <div className="border rounded">
+      <p className="p-2 border-b"> CHARACTER STATS </p>
+      {attackStat}
+      {defenseStat}
+      {speedStat}
+      {kiStat}
+
+    </div>
   );
 }
 
