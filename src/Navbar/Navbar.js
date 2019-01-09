36876d7b7e82cc358f9d2fa290f0f202b52eb4d8
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-09 18:00:35 -0500

    Added logo and styling to Navbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,23 +1,35 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
 
-const linkStyle = "text-blue hover:text-blue-darker no-underline hover:underline";
+const linkStyle = "text-grey hover:text-white no-underline";
 
 const Navbar = () => (
-  <nav className="container mx-auto">
-    <ul className="list-reset flex m-4">
-      <li className="mr-6">
-        <NavLink to="/" className={linkStyle}>
-          Home
+  <div className="pin-t sticky z-50 mb-4 shadow bg-blue-darker">
+    <div class="container  mx-auto">
+    <nav className="py-4 h-16 md:h-24  flex items-center justify-between">
+      <div className="h-full">
+        <NavLink to="/">
+        <img 
+          src={require('../images/dbzlf2-logo.png')}
+          className="h-full"
+        />
         </NavLink>
-      </li>
-      <li>
-        <NavLink to="/characters/" className={linkStyle}>
-          Characters
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
+      </div>
+      <ul className="list-reset flex m-4">
+        <li className="mr-6">
+          <NavLink to="/" className={linkStyle}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/characters/" className={linkStyle}>
+            Characters
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  </div>
 );
 
 export default Navbar;
