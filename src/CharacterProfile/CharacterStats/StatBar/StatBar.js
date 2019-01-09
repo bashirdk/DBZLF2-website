f33dfa592ac93503cc4c3784cb613ef15ff51413
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-09 11:28:46 -0500

    Updated the stat bars to display proper stats and colours

--- src/CharacterProfile/CharacterStats/StatBar/StatBar.js
+++ src/CharacterProfile/CharacterStats/StatBar/StatBar.js
@@ -3,29 +3,43 @@ import React from "react";
 const StatBar = ({ statName, stat }) => {  
 
   const barSpan = (bgColor, statWidth) => (
-    <span className={bgColor + " inline-block h-full"} style={statWidth}></span>
+    <span className={bgColor + " inline-block h-full absolute"} style={statWidth}></span>
   );
 
+  var statBars = [];
+
+  if (statName !== "Ki Usage" && stat > 30)
+  {
+    statBars.push(barSpan("bg-red", {width: "100%"}));
+    statBars.push(barSpan("bg-yellow", {width: "100%"}));
+    statBars.push(barSpan("bg-green", {width: "100%"}));
+    statBars.push(barSpan("bg-blue", {width: (stat-30)*10+"%"}));
+  }
+  else if (statName !== "Ki Usage" && stat > 20)
+  {
+    statBars.push(barSpan("bg-red", {width: "100%"}));
+    statBars.push(barSpan("bg-yellow", {width: "100%"}));
+    statBars.push(barSpan("bg-green", {width: (stat-20)*10+"%"}));
+  }
+  else if (statName !== "Ki Usage" && stat > 10)
+  {
+    statBars.push(barSpan("bg-red", {width: "100%"}));
+    statBars.push(barSpan("bg-yellow", {width: (stat-10)*10+"%"}));
+  }
+  else if (statName !== "Ki Usage" && stat <= 10)
+  {
+    statBars.push(barSpan("bg-red", {width: stat*10+"%"}));
+  }
 
   if (statName === "Ki Usage")
   {
     stat = ((stat/250)* 100) + "%";
+    statBars.push(barSpan("bg-red", {width: stat}));
   }
-  else 
-  {
-    stat = stat + "%";
-  }
-
-  const statStyle = {
-    width: stat
-  }
-
-
-  const barUse = barSpan("bg-red", statStyle);
 
   return (
     <div className="mx-2 w-full h-full bg-grey-lighter relative">
-      {barUse}
+      {statBars}
     </div>
   );
   
