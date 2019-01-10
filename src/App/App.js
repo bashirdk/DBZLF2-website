9a02f3668d32b1d19d88244e802a0ce8661e14ec
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-10 16:49:24 -0500

    Built a home page component

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import ScrollToTop from "../ScrollToTop";
 
+import Home from "../Home";
 import CharacterList from "../CharacterList";
 import CharacterProfile from "../CharacterProfile";
 import Navbar from "../Navbar";
 import React from "react";
 
-const Index = () => <h2>Home</h2>;
 const NoMatch = () => <h1 className="text-white text-center my-24">404: Page not found</h1>
 
 const App = () => (
@@ -21,7 +21,7 @@ const App = () => (
             <Switch>            
               <Route path="/characters/:saga/:name" component={CharacterProfile} />
               <Route path="/characters/" exact component={CharacterList} />
-              <Route path="/" exact component={Index} />
+              <Route path="/" exact component={Home} />
               <Route component={NoMatch} />            
             </Switch>
           </ScrollToTop>
---
+++ src/Home/Home.js
@@ -0,0 +1,26 @@
+import "./Home.css";
+
+import React from "react";
+
+const Home = () => (
+  <div>  
+    <h1 className="text-white">DBZ LF2</h1>
+    <div className="relative mb-10">
+      <img 
+        src={require('../images/dbzlf2-logo-horizontal.png')}
+        className="hidden sm:block"
+      />
+      <img 
+        src={require('../images/dbzlf2-logo.png')}
+        className="block sm:hidden"
+      />
+      <p className="version-label text-white text-right -mt-5 mr-2 sm:-mt-3 lg:-mt-8 lg:mr-10"> Ver 0.2.8 </p>
+    </div>
+    
+    <div className="text-center mt-24">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/qr8AJyuS-VY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+);
+
+export default Home;
