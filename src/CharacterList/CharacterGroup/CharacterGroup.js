24122d1b2e8462897155a6eb66be87f484f21cfd
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-10 16:56:22 -0500

    Display message when no characters exist in saga group

--- src/CharacterList/CharacterGroup/CharacterGroup.js
+++ src/CharacterList/CharacterGroup/CharacterGroup.js
@@ -2,9 +2,14 @@ import CharacterCard from "../CharacterCard";
 import React from "react";
 
 const CharacterGroup = ({ data: { name, characters } }) => {
-  const cards = characters.map((character) => (
+  var cards = characters.map((character) => (
     <CharacterCard key={character.id} character={character} />
   ));
+
+  if (cards.length == 0)
+  {
+    cards = <p className="text-white italic">Nothing to see here...</p>
+  }
 
   return (
     <div className="border-2 lf2-border-blue lf2-bg-blue rounded my-5 px-6 py-5 mx-5 lg:mx-0">
