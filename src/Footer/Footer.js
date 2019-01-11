469c009f33c8fb1f25fbbd317f81b58128c59a3b
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-01-11 01:26:13 -0500

    Made the footer component more responsive

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,24 +6,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Footer = () => (
   <div className="lf2-bg-blue border-t-2 lf2-border-blue mt-12">
     <div class="container mx-auto">
-      <footer className="py-10">  
-        <div className="flex items-center justify-between">
-          <div className="text-white">
+      <footer className="py-10 mx-3">  
+        <div className="md:flex items-center justify-between">
+          <div className="text-center md:text-left text-white">
             <p className="my-3"> Dragon Ball Z © 2003 BIRD STUDIO/ SHUEISHA, TOEI ANIMATION. </p>
             <p className="my-3"> Little Fighter 2 © 1999-2008, Marti Wong & Starsky Wong, All rights reserved.</p>
           </div>
-          <div class="text-white">
-            <ul>
-              <a href="https://www.youtube.com/user/dbzlf2" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
-                <FontAwesomeIcon icon={['fab', "youtube"]} size="2x" color="white" /> 
-              </a>
-              <a href="http://dbzlf2.blogspot.com/" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
-                <FontAwesomeIcon icon={['fab', "blogger"]} size="2x" color="white" /> 
-              </a>
-              <a href="https://discord.gg/mPR3Vr5" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
-                <FontAwesomeIcon icon={['fab', "discord"]} size="2x" color="white" /> 
-              </a>          
-            </ul>
+          <div class="text-center my-5 md:text-left text-white">
+            <a href="https://www.youtube.com/user/dbzlf2" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
+              <FontAwesomeIcon icon={['fab', "youtube"]} size="2x" color="white" /> 
+            </a>
+            <a href="http://dbzlf2.blogspot.com/" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
+              <FontAwesomeIcon icon={['fab', "blogger"]} size="2x" color="white" /> 
+            </a>
+            <a href="https://discord.gg/mPR3Vr5" target="_blank" rel="noopener noreferrer" className="fa-icon m-1">
+              <FontAwesomeIcon icon={['fab', "discord"]} size="2x" color="white" /> 
+            </a>          
           </div>
         </div>
       </footer>
