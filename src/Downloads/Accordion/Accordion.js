3f6f94d721cf19a525e318fb249e25884dc23d66
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-07-14 20:41:55 -0400

    clean up accordion buttons

--- src/Downloads/Accordion/Accordion.js
+++ src/Downloads/Accordion/Accordion.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import Chevron from "./Chevron";
 
 import "./Accordion.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Accordion(props) {
   const [setActive, setActiveState] = useState("");
@@ -21,30 +22,25 @@ function Accordion(props) {
   }
 
   return (
-    <div className="accordion__section border-2 lf2-border-blue lf2-bg-blue rounded text-white">
+    <div className="accordion__section border-2 lf2-border-blue lf2-bg-blue my-5 rounded text-white">
+      
       <button className={`accordion ${setActive} px-6 py-5 text-white`} onClick={toggleAccordion}>       
         
         <div className="flex w-full">
-
           <div className="w-1/3 text-left inline-block">
             <Chevron className={`${setRotate}`} width={10} fill={"#fff"} />
-            <h2 className="inline-block"> DBZ LF2 {props.title} </h2> 
+            <h2 className="inline-block"> DBZ LF2 {props.version} </h2> 
+          </div>          
+          <div className="w-1/3 text-center">
+          <p class="my-1"> {props.date} </p>
           </div>
-          
-        <div className="w-1/3 text-center">
-         <p class="my-2"> {props.date} </p>
+          <div className="w-1/3 text-right">
+            <p class="my-1"> 
+              Characters: <span class="rounded-lg py-05 px-2 my-2 mr-2 bg-green"> {props.charactersTotal} </span>
+              New: <span class="rounded-lg py-05 px-2 my-2 bg-blue"> {props.charactersNew} </span>
+            </p>  
+          </div>
         </div>
-
-        <div className="w-1/3 text-right">
-          <p class="my-2"> 
-            Characters: <span class="rounded-lg py-05 px-2 my-2 mr-2 bg-green"> {props.charactersTotal} </span>
-            New: <span class="rounded-lg py-05 px-2 my-2 bg-blue"> {props.charactersNew} </span>
-          </p>  
-        </div>
-
-        </div>
-        
-         
 
       </button>
       
@@ -52,12 +48,34 @@ function Accordion(props) {
         ref={content}
         style={{ maxHeight: `${setHeight}` }}
         className={`accordion__content ${setActive} lf2-border-blue lf2-bg-blue rounded px-6 mx-5 lg:mx-0 text-white`}
-      >
-        <div
-          className="accordion__text"
-          dangerouslySetInnerHTML={{ __html: props.content }}
-        />
+      >      
+        <div className="pb-3" >
+
+          <div className="text-center">
+            <a href={"https://github.com/bashirdk/dbz-lf2/releases/download/" + props.version + "/DBZ.LF2." + props.version + ".zip"}>
+              <button class="download-button">
+                <FontAwesomeIcon icon="download" color="white" />
+                <span class="mx-2">DOWNLOAD</span>
+              </button>
+            </a>
+          </div>
+
+          <div class="text-center">
+            { props.trailer ? 
+              <div class="download-trailer">
+                <div class="video-responsive">
+                  <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + props.trailer} frameborder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                  </iframe> 
+                </div>
+              </div>
+              :
+            <div></div> }
+          </div>
+
+        </div>
       </div>
+
     </div>
   );
 }
--- src/Downloads/Downloads.js
+++ src/Downloads/Downloads.js
@@ -18,45 +18,12 @@ class Downloads extends Component {
 			<div>
 
 			<Accordion 
-				title={download.version}
+				version={download.version}
 				date={download.date}
 				charactersTotal={download.characters.total}
 				charactersNew={download.characters.new}
-				content="deez nuts"
+				trailer={download.trailer}
 			/>			
-
-			<div className="border-2 lf2-border-blue lf2-bg-blue rounded my-5 px-6 py-5 mx-5 lg:mx-0 text-white">
-				<div class="sm:w-1 md:w-1/2 inline-block align-top"> 
-					<h2> DBZ LF2 {download.version} </h2>
-					<a href={"https://github.com/bashirdk/dbz-lf2/releases/download/" + download.version + "/DBZ.LF2." + download.version + ".zip"}>
-						<button class="download-button">
-						  <FontAwesomeIcon icon="download" color="white" />
-							<span class="mx-2">DOWNLOAD</span>
-						</button>
-					</a>
-					
-					
-				</div>
-				<div class="sm:w-1 md:w-1/2 inline-block text-right align-top">
-					<p class="my-2"> Release Date: {download.date}</p>
-					<p class="my-2"> Total Characters: <span class="rounded-lg py-05 px-2 my-2 bg-green"> {download.characters.total} </span></p> 
-					<p class="my-2"> New Characters: <span class="rounded-lg py-05 px-2 my-2 bg-blue"> {download.characters.new} </span> </p>
-				</div>
-				
-				<div class="text-center">
-					{ download.trailer ? 
-						<div class="download-trailer">
-							<div class="video-responsive">
-								<iframe width="560" height="315" src={"https://www.youtube.com/embed/" + download.trailer} frameborder="0" 
-									allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-								</iframe> 
-							</div>
-						</div>
-						:
-					<div></div> }
-				</div>
-				
-			</div>
 
 			</div>
 		));
