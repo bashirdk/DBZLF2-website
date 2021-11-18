7f118b2c702eeb9b9eed89d4c8c9e37e276b5ccc
Bashir Duranai-Khan <bashirkdk@gmail.com>
2021-11-18 02:01:48 -0500

    add Little Fighter 2 SVG animation to nav and home page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import React from "react";
 
 import DragonBall from "../Logo/DragonBall";
+import LittleFighter2 from "../Logo/LittleFighter2";
 
 import "./Navbar.css";
 
@@ -10,18 +11,19 @@ const linkStyle = "navbar-style text-grey hover:text-white no-underline";
 const Navbar = () => (
   <div className="pin-t sticky z-50 mb-4 shadow lf2-bg-blue border-b-2 lf2-border-blue">
     <div className="container mx-auto">
-      <nav className="py-4 h-16 md:h-24  flex items-center justify-between mx-3">
+      <nav className="py-4 h-24 flex items-center justify-between mx-3">
         <div className="h-full">
           <NavLink to="/">
           <DragonBall
             strokeWidth="5"
             width="132"
             height="35"
-          />
-          <img 
-            src={require('../../images/dbzlf2-logo.png')}
-            className="h-full"
-            alt="dbz lf2 logo"
+          /> <br/>
+          <LittleFighter2
+            strokeWidth="3"
+            stroke="rgba(0,0,0,1)"
+            width="132"
+            height="35"
           />
           </NavLink>
         </div>
