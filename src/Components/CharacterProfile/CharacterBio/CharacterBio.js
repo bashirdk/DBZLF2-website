637d8879c138950fdf61aca94f7f7cab889b3c3f
Bashir Duranai-Khan <bashirkdk@gmail.com>
2024-03-28 14:34:42 -0400

    add battle damage outfits

--- src/Components/CharacterProfile/CharacterBio/CharacterBio.js
+++ src/Components/CharacterProfile/CharacterBio/CharacterBio.js
@@ -11,23 +11,65 @@ const CharacterBio = ({ character }) => {
     <span className={`${labelStyle} ${race.toLowerCase()}-race-label`}> {race} </span>
   ));
 
+  const hasBattleDamage = () => {
+    try {
+      return (
+      <img 
+        src={require(`../../../images/profile/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}-damage.gif`)}
+        alt={`standing pic of ${character.name}`}
+        className="w-full absolute pin-t pin-l hidden"
+        id="character_standing_damage"
+      /> )
+    } catch(err) {
+      return false;
+    }
+  }
+
+  const swapDamage = () => {
+    const char = document.querySelector('#character_standing');
+    const charDamage = document.querySelector('#character_standing_damage');
+    const buttonDamage = document.querySelector('#btn_battle_damage');
+
+    if (char && char.classList && char.classList.contains('hidden')) {
+      buttonDamage.innerHTML = 'View Battle Damage Outfit';
+      char.classList.add('block');
+      char.classList.remove('hidden');
+      charDamage.classList.add('hidden');
+      charDamage.classList.remove('block');
+    } else {
+      buttonDamage.innerHTML = 'View Normal Outfit';
+      char.classList.add('hidden');
+      char.classList.remove('block');
+      charDamage.classList.add('block');
+      charDamage.classList.remove('hidden');
+    }
+  }
+
   return (
     <div className="border-2 rounded lf2-bg-blue lf2-border-blue px-5 py-5">
       <div className="relative">
         <img src={require(`../../../images/profile/${character.saga.toLowerCase()}_bg.png`)}
-          alt={`standing pic of ${character.name}`}
+          alt={`background image for ${character.saga.toLowerCase()} saga`}
           className="w-full"
         />
-      <img src={require(`../../../images/profile/s.png`)}
-        alt={`standing pic of ${character.name}`}
-        className="w-full absolute pin-t pin-l"
-      />
+        <img src={require(`../../../images/profile/s.png`)}
+          alt={`shadow`}
+          className="w-full absolute pin-t pin-l"
+        />
         <img 
           src={require(`../../../images/profile/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.gif`)}
           alt={`standing pic of ${character.name}`}
-          className="w-full absolute pin-t pin-l"
+          className="w-full absolute pin-t pin-l block"
+          id="character_standing"
         />
+        { hasBattleDamage() }
       </div>
+
+     <div>
+       { hasBattleDamage() ? 
+        <p class="text-white text-center"><button onClick={swapDamage} class="text-white" id="btn_battle_damage">View Battle Damage Outfit</button></p>
+        : ""}
+     </div>
 
       <div className="mt-3">
         <h4 className="text-white"> Race </h4> 
@@ -38,7 +80,7 @@ const CharacterBio = ({ character }) => {
         <h4 className="text-white"> Saga </h4> 
         <hr className={hrStyle} />
         <span className={`${labelStyle} ${character.saga.toLowerCase()}-saga-label`}> {character.saga} </span>
-      </div>      
+      </div>
       
     </div>
   );
