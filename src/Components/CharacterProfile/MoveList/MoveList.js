1f43244c9558f57fec36919dabdd160d4d960ec6
Bashir Duranai-Khan <bashirkdk@gmail.com>
2024-06-20 16:59:21 -0400

    update downloads page and add new goku chars

--- src/Components/CharacterProfile/MoveList/MoveList.js
+++ src/Components/CharacterProfile/MoveList/MoveList.js
@@ -7,6 +7,10 @@ const MoveList = ({ character }) => {
   const moveNameStyle = "lf2-bg-blue-dark text-white w-full block sm:w-1/2 md:w-2/5 md:border-b-0 px-2 py-3 align-middle";
   const moveInputStyle = "w-full text-white block sm:w-1/2 md:w-3/5 px-2 py-3 align-middle";
 
+  const comboContainerStyle = "border-b lf2-border-blue block";
+  const comboNameStyle = "lf2-bg-blue-dark text-white w-full block md:border-b-0 px-2 py-3 align-middle";
+  const comboInputStyle = "w-full text-white block px-2 py-3 align-middle";
+
   const subMoveList = ( input ) => {
     if (input.length === 0)
     {
@@ -81,8 +85,43 @@ const MoveList = ({ character }) => {
     }
   };
 
+  const comboMoveList = ( input ) => {
+    if (input.length === 0)
+    {
+      return (
+        <div className={moveContainerStyle}>
+        <div className="px-2 py-3 text-white"> 
+          <i> None Available </i>
+        </div>
+      </div>  
+      )
+    } else {
+      const mapMoves = input.map((mappedMove) => {
+        if (mappedMove.ultimate)
+        {
+          var ultimateMove = (
+            <span className="inline-block text-xs rounded px-2 py-1 bg-red-dark font-bold text-white my-0 md:mt-2 lg:my-0">
+              ULTIMATE
+            </span>
+          )          
+        }
+        return(
+        <div className={comboContainerStyle}>
+          <div className={comboNameStyle}> 
+            {mappedMove.name} {ultimateMove}
+          </div>
+          <div className={comboInputStyle}> 
+            {mappedMove.input}
+          </div>
+        </div> )
+      });
+      return mapMoves;
+    }
+  };
+
   const basicMoves = subMoveList(character.moves.basic);
   const specialMoves = subMoveList(character.moves.special);
+  // const combos = comboMoveList(character.moves.combos);
   const teamMoves = teamMoveList(character.moves.team);
   const counterAttacks = subMoveList(character.moves.counter);
   const transformations = subMoveList(character.moves.transformations);
@@ -96,6 +135,7 @@ const MoveList = ({ character }) => {
 
       <h3 className={'bg-blue ' + moveHeaderStyle } > Basic Moves </h3> {basicMoves}
       <h3 className={'bg-red ' + moveHeaderStyle}> Special Moves </h3> {specialMoves}
+      {/* <h3 className={'bg-blue-dark ' + moveHeaderStyle}> Combos </h3> {combos} */}
       <h3 className={'bg-grey ' + moveHeaderStyle}> Team Attacks </h3> {teamMoves}
       <h3 className={'bg-green ' + moveHeaderStyle}> Counter Attacks </h3> {counterAttacks}
       <h3 className={'bg-yellow-dark ' + moveHeaderStyle}> Transformations </h3> {transformations}
--- src/Components/Downloads/Accordion/Accordion.js
+++ src/Components/Downloads/Accordion/Accordion.js
@@ -60,6 +60,25 @@ function Accordion(props) {
     }
   }
 
+  function downloadsCharBgList(propsData) {
+    if (propsData) {
+      console.log('propsData', propsData)
+      return propsData.map(
+        (char) => {
+          return (
+            char.new ?
+            <li> {char.name} <span className="new"> NEW </span> </li>
+            :
+            <li> {char.name} </li>
+          )
+        }
+      )
+    }
+    else {
+      return ''
+    }
+  }
+
   let characters = mapUpdatesData(props.updates.major_updates.characters);
   let backgrounds = mapUpdatesData(props.updates.major_updates.backgrounds);
   let list_of_major_updates = mapUpdatesData(props.updates.major_updates.list_of_major_updates);
@@ -70,22 +89,39 @@ function Accordion(props) {
   let new_moves = mapUpdatesData(props.updates.minor_updates.new_moves);
   let speed_adjustments = mapUpdatesData(props.updates.minor_updates.speed_adjustments);
 
+  let characterListSaiyan = downloadsCharBgList(props.characterList.saiyan);
+  let characterListNamek = downloadsCharBgList(props.characterList.namek);
+  let characterListAndroid = downloadsCharBgList(props.characterList.android);
+  let characterListBuu = downloadsCharBgList(props.characterList.buu);
+
+  let backgroundListSaiyan = downloadsCharBgList(props.backgroundList.saiyan);
+  let backgroundListNamek = downloadsCharBgList(props.backgroundList.namek);
+  let backgroundListAndroid = downloadsCharBgList(props.backgroundList.android);
+  let backgroundListBuu = downloadsCharBgList(props.backgroundList.buu);
+  let backgroundListLf2 = downloadsCharBgList(props.backgroundList.lf2);
+
   return (
     <div className="accordion__section border-2 lf2-border-blue lf2-bg-blue my-5 rounded text-white">
 
       <button className={`accordion ${setActive} px-6 py-5 text-white`} onClick={toggleAccordion}>        
-        <div className="flex w-full">
-          <div className="w-1/3 text-left inline-block">
+        <div className="sm:inline-block lg:flex w-full">
+          <div className="lg:w-1/3 text-left inline-block float-left">
             <Chevron className={`${setRotate}`} width={10} fill={"#fff"} />
             <h2 className="inline-block"> DBZ LF2 {props.version} </h2> 
           </div>          
-          <div className="w-1/3 text-center">
-          <p className="my-1"> {props.date} </p>
+          <div className="lg:w-1/3 text-center inline-block">
+            <p className="my-2"> {props.date} </p>
           </div>
-          <div className="w-1/3 text-right">
-            <p className="my-1"> 
-              Characters: <span className="rounded-lg py-05 px-2 my-2 mr-2 bg-green"> {props.charactersTotal} </span>
+          <div className="lg:w-1/3 text-center sm:block">
+            <p className="mt-2"> 
+              Chars: <span className="rounded-lg py-05 px-2 my-2 mr-2 bg-green"> {props.charactersTotal} </span>
               New: <span className="rounded-lg py-05 px-2 my-2 bg-blue"> {props.charactersNew} </span>
+            </p>  
+          </div>
+          <div className="lg:w-1/3 text-center sm:block">
+            <p className="mt-2"> 
+              BGs: <span className="rounded-lg py-05 px-2 my-2 mr-2 bg-green"> {props.backgroundsTotal} </span>
+              New: <span className="rounded-lg py-05 px-2 my-2 bg-blue"> {props.backgroundsNew} </span>
             </p>  
           </div>
         </div>
@@ -107,6 +143,12 @@ function Accordion(props) {
               </button>
             </a>
              :
+             props.download === false ?
+             <button className="download-button" disabled>
+                <FontAwesomeIcon icon="download" color="white" />
+                <span className="mx-2">DOWNLOAD</span>
+              </button>
+              :
              <a href={"https://github.com/bashirdk/dbz-lf2/releases/download/" + props.version + "/DBZ.LF2." + props.version + ".zip"}>
              <button className="download-button">
                 <FontAwesomeIcon icon="download" color="white" />
@@ -115,6 +157,44 @@ function Accordion(props) {
             </a>}
             {/* <AdComponent /> */}
           </div>
+
+          <table className="hidden lg:table downloads-lists">
+            <tr>
+              <th colspan="4" className="text-lg py-1">Characters</th>
+            </tr>
+            <tr>
+              <th width="25%">Saiyan</th>
+              <th width="25%">Namek</th>
+              <th width="25%">Android</th>
+              <th width="25%">Buu</th>
+            </tr>
+            <tr>
+              <td> {characterListSaiyan.length === 0 ? <p className="text-center">N/A</p> : <ol> {characterListSaiyan}</ol>} </td>
+              <td> {characterListNamek.length === 0 ? <p className="text-center">N/A</p> : <ol> {characterListNamek}</ol>} </td>
+              <td> {characterListAndroid.length === 0 ? <p className="text-center">N/A</p> : <ol> {characterListAndroid}</ol>} </td>
+              <td> {characterListBuu.length === 0 ? <p className="text-center">N/A</p> : <ol> {characterListBuu}</ol>} </td>
+            </tr>
+          </table>
+
+          <table className="hidden lg:table downloads-lists">
+            <tr>
+              <th colspan="5" className="text-lg py-1">Backgrounds</th>
+            </tr>
+            <tr>
+              <th width="20%">Saiyan</th>
+              <th width="20%">Namek</th>
+              <th width="20%">Android</th>
+              <th width="20%">Buu</th>
+              <th width="20%">LF2</th>
+            </tr>
+            <tr>
+              <td> {backgroundListSaiyan.length === 0 ? <p className="text-center">N/A</p> : <ol> {backgroundListSaiyan}</ol>} </td>
+              <td> {backgroundListNamek.length === 0 ? <p className="text-center">N/A</p> : <ol> {backgroundListNamek}</ol>} </td>
+              <td> {backgroundListAndroid.length === 0 ? <p className="text-center">N/A</p> : <ol> {backgroundListAndroid}</ol>} </td>
+              <td> {backgroundListBuu.length === 0 ? <p className="text-center">N/A</p> : <ol> {backgroundListBuu}</ol>} </td>
+              <td> {backgroundListLf2.length === 0 ? <p className="text-center">N/A</p> : <ol> {backgroundListLf2}</ol>} </td>
+            </tr>
+          </table>
 
           <h3 className="mb-1">Major Updates</h3>
           <div className="mx-3">
@@ -131,7 +211,7 @@ function Accordion(props) {
             {outputIfExists("New Moves", new_moves)}
             {outputIfExists("Speed Adjustments", speed_adjustments)}
             {outputIfExists("Other Updates", list_of_minor_updates)}
-          </div>     
+          </div>
           <br/>
           <div className="text-center">
             { props.trailer ? 
