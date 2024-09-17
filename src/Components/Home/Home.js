fe7c9f8588a7ee0c8ac120f36084335ce275a7a9
bashirdk <bashirkdk@gmail.com>
2024-09-17 19:07:39 -0400

    add countdown and budokai bgs (#37)
    * add countdown and budokai bgs
    
    * add content to header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -25,6 +25,38 @@ const versionVariants = {
   
 }
 
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sept 28, 2024 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
 const Home = () => (
   <div>  
     <Helmet>
@@ -51,13 +83,21 @@ const Home = () => (
       </motion.div>
     </div>
 
-    <p className="text-white text-center"> Project has been revived thanks to <b><i>NetaFelizZz/Excellent Approximation/Rancro</i></b> (updated sprites/data + finished SS Trunks and made Ultra Trunks and will continue the mod with my help on the side) </p>
+    <div className="text-center text-white">
+      <p className="mb-5 text-3xl"> DBZ LF2 v0.4.3 RELEASE </p>
+      <p className="mb-5 text-4xl"> September 28, 2024 </p>
+      <p className="mb-5 text-4xl"> AVAILABLE IN </p>
+      <h3 className="mb-5 text-5xl" id="counter">COUNTDOWN</h3>
+    </div>
+    
+
+    {/* <p className="text-white text-center"> Project has been revived thanks to <b><i>NetaFelizZz/Excellent Approximation/Rancro</i></b> (updated sprites/data + finished SS Trunks and made Ultra Trunks and will continue the mod with my help on the side) </p>
     <br></br>
-    <p className="text-white text-center"> new EXE and cool features made possible by darkfiren's exe </p>
+    <p className="text-white text-center"> new EXE and cool features made possible by darkfiren's exe </p> */}
     
     <div className="text-center mt-12" >
       <div className="video-responsive">
-        <iframe title="trailer" width="100%" height="315" src="https://www.youtube.com/embed/UjgQmvsjQY4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe title="trailer" width="100%" height="315" src="https://www.youtube.com/embed/n7KXL2mi9tM" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
     </div>
   </div>
