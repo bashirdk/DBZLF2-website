6a129ee2bb306b97ff4904d64ed75886be688f1f
Bashir Duranai-Khan <bashirkdk@gmail.com>
2024-09-25 21:59:06 -0400

    add face pixel art pics on hover on character list

--- src/Components/CharacterList/CharacterCard/CharacterCard.js
+++ src/Components/CharacterList/CharacterCard/CharacterCard.js
@@ -5,13 +5,18 @@ import React from "react";
 
 const CharacterCard = ({ character }) => (
   <Link to={`/characters/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}`}>
-    <div className="character-card inline-block mx-1 my-1 w-48 overflow-hidden">
+    <div className="character-card inline-block mx-1 my-1 w-48 overflow-hidden relative">
       <img 
         src={require(`../../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.png`)}
         alt={`face pic of ${character.name}`}
-        className="w-full"
+        className="w-full anime-face"
       />
-      <div className="text-center text-sm px-1 py-2">
+      <img 
+        src={require(`../../../images/face/${character.saga.toLowerCase()}/${character.url_id.toLowerCase()}.bmp`)}
+        alt={`face pic of ${character.name}`}
+        className="w-full pixel-face"
+      />
+      <div className="card-text text-center text-sm px-1">
         <p>{character.name}</p>
       </div>
     </div>
